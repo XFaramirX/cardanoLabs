3d14b0032dc9ea9ba9850b33dcffde9e698489d8
DESKTOP-QSVLL54\David <xfaramir@gmail.com>
2021-04-14 13:42:57 -0500

    clean up apollo-test-tools

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,3 +1,14 @@
+const { ApolloError } = require("apollo-server");
+
+// Firebase App (the core Firebase SDK) is always required and must be listed before other Firebase SDKs
+const admin = require("firebase-admin");
+const serviceAccount = require("../../service-account-file.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://angelbernalesquivel-ecf4b-default-rtdb.firebaseio.com",
+});
+
 const resolvers = {
   Query: {
     async images() {
